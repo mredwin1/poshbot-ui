fe0a10f98958d6ab9cd6729ae562cec2b232b960
Edwin Cruz <ecruz1113@gmail.com>
2022-04-04 00:22:39 -0400

    Install react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const container = document.getElementById('root');
