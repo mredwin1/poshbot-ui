c3f5959eebdb2e9204ecf4c512cbb775b64ce25f
Edwin Cruz <ecruz1113@gmail.com>
2022-04-03 20:45:13 -0400

    Add NavBar

---
+++ src/components/navBar.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              PoshBot
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">
+                  PoshUsers
+                </a>
+                <a className="nav-link" href="#">
+                  Listings
+                </a>
+                <a className="nav-link" href="#">
+                  Campaigns
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavBar;
