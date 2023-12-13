b826bf270b14c2a3c1f3533214e99d9cad28d217
Edwin Cruz <55803329+mredwin1@users.noreply.github.com>
2023-12-12 19:48:06 -0500

    Update api.js to use new production backend

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -11,7 +11,7 @@ const api =
     const { user } = getState();
     const baseURL =
       process.env.NODE_ENV === 'production'
-        ? 'https://turtleswags.com'
+        ? 'https://api.melondova.com'
         : 'http://localhost:8000';
     if (user.accessToken) {
       axios.defaults.headers.common[
